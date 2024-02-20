3044570f78b3b31c84e0297e8ef14453dd43e7e2
Joanna Sokolowska <joanna_s@op.pl>
2024-02-20 11:15:37 +0100

    User Schema updated to adjust its columns to SPring Security requirements

---
+++ src/main/resources/db/2024-02-20/02-modify-users.sql
@@ -0,0 +1,13 @@
+--liquibase formatted sql
+--changeset jasokolowska:10
+ALTER TABLE users
+    ADD COLUMN enabled BOOLEAN;
+
+--changeset jasokolowska:11
+UPDATE users
+SET enabled = true;
+
+--changeset jasokolowska:12
+ALTER TABLE users
+    ALTER COLUMN enabled SET NOT NULL,
+    ALTER COLUMN enabled SET DEFAULT true;
